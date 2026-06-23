7af2958905774b3b09b329dde11fe59386b2dd93
manish <support@technofra.com>
2026-06-23 10:39:29 +0530

    uwantend dropdown remove

--- resources/views/admin/barcodes/generate.blade.php
+++ resources/views/admin/barcodes/generate.blade.php
@@ -37,8 +37,8 @@
                             <label for="barcodeFormat" class="form-label fw-semibold">Barcode Format</label>
                             <select id="barcodeFormat" class="form-select form-select-lg">
                                 <option value="code128" selected>Code 128 - Default</option>
-                                <option value="code39">Code 39</option>
-                                <option value="ean13">EAN-13</option>
+                                {{-- <option value="code39">Code 39</option> --}}
+                                {{-- <option value="ean13">EAN-13</option> --}}
                             </select>
                         </div>
 
@@ -434,3 +434,4 @@
         })();
     </script>
 @endpush
+
